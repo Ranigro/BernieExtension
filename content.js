1c575ef12f0b24696ac6b48a9ca60f9bcb022624
ranigro <rannigro@gmail.com>
2021-03-28 23:14:58 -0400

    got it to detect a low font size and bump that shit

--- content.js
+++ content.js
@@ -6,8 +6,10 @@ it is too small
 console.log("Chrome extension go/");
 let paragraphs = document.getElementsByTagName('p');
 for (elt of paragraphs){
+     var style = window.getComputedStyle(elt, null).getPropertyValue('font-size');
+     var fontSize = parseFloat(style);
      elt.style['background-color'] = '#FF00FF';
-     if (elt.style['font-size'] < '-1000px') {
+     if (fontSize < 20) {
         elt.style['font-size'] = '20px';
      }
 }
