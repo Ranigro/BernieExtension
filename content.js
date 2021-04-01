022d7092073775c79fb4f5962cd2bce8d187c48c
ranigro <rannigro@gmail.com>
2021-03-31 23:03:39 -0400

    first commit

--- content.js
+++ content.js
@@ -4,16 +4,6 @@ I want to try to make it possible to access the font size and make it bigger if
 it is too small
 */
 console.log("Chrome extension go/");
-let paragraphs = document.getElementsByTagName("p");
-for (elt of paragraphs){
-     var style = window.getComputedStyle(elt, null).getPropertyValue('font-size');
-     var fontSize = parseFloat(style);
-     elt.style['background-color'] = '#FF00FF';
-     if (fontSize < 20) {
-        elt.style['font-size'] = '20px';
-     }
-}
-
 /*
 This grabs all of the static imgs that load and turn them into whichever image I put
 down there
